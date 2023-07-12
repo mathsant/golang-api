1ff1e4c1221effb9a33b52592d5cb665175f3d99
Matheus Silva <mathsant_99@hotmail.com>
2023-07-12 17:36:33 -0300

    adding route to create product

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,30 @@
 package main
 
-import "github.com/mathsant/golang-api/configs"
+import (
+	"net/http"
+
+	"github.com/mathsant/golang-api/configs"
+	"github.com/mathsant/golang-api/internal/entity"
+	"github.com/mathsant/golang-api/internal/infra/database"
+	"github.com/mathsant/golang-api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProductDB(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
 }
